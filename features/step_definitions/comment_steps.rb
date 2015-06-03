17379b33869fc2b25ae90bfc055168f2f2c227d1
Chai Feng <chaifeng@odd-e.com>
2015-06-03 22:03:07 +0800

    Adding comment steps

---
+++ features/step_definitions/comment_steps.rb
@@ -0,0 +1,10 @@
+@current_time = ""
+
+当(/^我在评论文本框里输入 "(.*?)"$/) do |arg1|
+  @current_time = Time.now.to_s
+  step %Q[我在 id 为 "comment" 文本框里输入 "#{arg1}|#{@current_time}"]
+end
+
+那么(/^我会看到评论 "(.*?)"$/) do |arg1|
+  step %Q[我会看到页面上的文本 "#{arg1}|#{@current_time}"]
+end
