f22d52a4e6ba08396f02306120c301b548007f54
Chai Feng <chaifeng@odd-e.com>
2015-06-03 12:49:14 +0800

    Adding init steps

---
+++ features/step_definitions/my_first_steps.rb
@@ -0,0 +1,21 @@
+# encoding: UTF-8
+
+假如(/^我打开了 URL "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+那么(/^我会看到页面的链接为 "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+那么(/^我会看到页面的标题为 "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+当(/^我在 id 为 "(.*?)" 文本框里输入 "(.*?)"$/) do |arg1, arg2|
+  pending # express the regexp above with the code you wish you had
+end
+
+当(/^我点击了 id 为 "(.*?)" 的按钮$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
