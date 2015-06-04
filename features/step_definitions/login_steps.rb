9e4c9e5bb33a2bf03e1181bc47c6019ba8480eed
Chai Feng <chaifeng@odd-e.com>
2015-06-04 21:50:30 +0800

    Pass the new login test

---
+++ features/step_definitions/login_steps.rb
@@ -0,0 +1,17 @@
+# encoding: UTF-8
+
+假如(/^存在用户 "(.*?)" 且他的密码是 "(.*?)"$/) do |arg1, arg2|
+  %x[ wp user create "#{arg1}" "#{arg1}@chaifeng.com" --role=contributor --user_pass="#{arg2}" ]
+end
+
+当(/^使用用户名 "(.*?)" 和密码 "(.*?)" 登录$/) do |arg1, arg2|
+  visit "http://atdd.local/wp-admin/"
+  fill_in "user_login", with: arg1
+  fill_in "user_pass", with: arg2
+  click_on "Sign In"
+end
+
+那么(/^能够成功登录$/) do
+  expect(title).to eq("Dashboard ‹ Specification By Example Workshop — WordPress")
+  %x[wp user delete jack --yes]
+end
