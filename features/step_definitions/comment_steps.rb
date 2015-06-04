d837157ffadd7a52594010e8900226f27047751c
Chai Feng <chaifeng@odd-e.com>
2015-06-05 07:43:28 +0800

    Pass the test, comment need to be approved

--- features/step_definitions/comment_steps.rb
+++ features/step_definitions/comment_steps.rb
@@ -16,3 +16,25 @@ end
 假如(/^批准了评论 "(.*?)"$/) do |arg1|
   %x[wp comment approve $(wp comment list --format=ids --search="#{arg1}|#{@current_time}")]
 end
+
+当(/^普通用户提交了一个评论$/) do
+  visit "http://atdd.local/wp-login.php"
+  fill_in "user_login", with: "marry"
+  fill_in "user_pass", with: "s3cr3t"
+  click_on "Sign In"
+
+  visit "http://atdd.local/?p=9"
+  fill_in "comment", with:"a comment by subscriber"
+  click_on "Post Comment"
+end
+
+那么(/^这个评论将等待被审核$/) do
+  comment_ids = %x[wp comment list --format=ids --post_id=9 --status=hold --search="a comment by subscriber"]
+  expect(comment_ids).not_to eq("")
+end
+
+那么(/^这个评论不会被其他人公开看到$/) do
+  click_on "Log out"
+  expect(page).to have_no_content("a comment by subscriber")
+  #{}%x[wp comment delete "$(wp comment list --format=ids --post_id=9 --status=hold --search="a comment by subscriber")" --force]
+end
