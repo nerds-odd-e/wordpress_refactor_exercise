b23e23cd1629b1bae1e46c4267321b3dd811ccc8
Chai Feng <chaifeng@odd-e.com>
2015-06-05 14:14:43 +0800

    extract variables, post_id & comment_content

--- features/step_definitions/base_methods.rb
+++ features/step_definitions/base_methods.rb
@@ -3,7 +3,7 @@ def create_user username, password
 end
 
 def login username, password
-  visit "http://atdd.local/wp-admin/"
+  visit "http://atdd.local/wp-login.php"
   fill_in "user_login", with: username
   fill_in "user_pass", with: password
   click_on "Sign In"
--- features/step_definitions/comment_steps.rb
+++ features/step_definitions/comment_steps.rb
@@ -1,31 +1,32 @@
-@current_time = ""
-
 当(/^普通用户提交了一个评论$/) do
   login "marry", "s3cr3t"
-  
-  visit "http://atdd.local/?p=9"
-  fill_in "comment", with:"a comment by subscriber"
+
+  @post_id = 9
+  @comment_content = "a comment by subscriber"
+
+  visit "http://atdd.local/?p=#{@post_id}"
+  fill_in "comment", with: @comment_content
   click_on "Post Comment"
 end
 
 那么(/^这个评论将等待被审核$/) do
-  comment_ids = %x[wp comment list --format=ids --post_id=9 --status=hold --search="a comment by subscriber"]
+  comment_ids = %x[wp comment list --format=ids --post_id=#{@post_id} --status=hold --search="#{@comment_content}"]
   expect(comment_ids).not_to eq("")
 end
 
 那么(/^这个评论不会被其他人公开看到$/) do
   click_on "Log out"
   expect(page).to have_no_content("a comment by subscriber")
-  %x[wp comment delete "$(wp comment list --format=ids --post_id=9 --status=hold --search="a comment by subscriber")" --force]
+  %x[wp comment delete "$(wp comment list --format=ids --post_id=#{@post_id} --status=hold --search="#{@comment_content}")" --force]
 end
 
 当(/^这个评论被编辑审核通过$/) do
-  %x[wp comment approve "$(wp comment list --format=ids --post_id=9 --status=hold --search="a comment by subscriber")"]
+  %x[wp comment approve "$(wp comment list --format=ids --post_id=#{@post_id} --status=hold --search="#{@comment_content}")"]
 end
 
 那么(/^这个评论将被其他人公开看到$/) do
   click_on "Log out"
   visit "http://atdd.local/?p=9"
   expect(page).to have_content("a comment by subscriber")
-  %x[wp comment delete "$(wp comment list --format=ids --post_id=9 --search="a comment by subscriber")" --force]
+  %x[wp comment delete "$(wp comment list --format=ids --post_id=#{@post_id} --search="#{@comment_content}")" --force]
 end
