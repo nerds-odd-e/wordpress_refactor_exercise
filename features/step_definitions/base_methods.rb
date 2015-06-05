e471a02a3ce42553a0755d32742c0e72ffbe083b
Chai Feng <chaifeng@odd-e.com>
2015-06-05 13:26:34 +0800

    move methods to a new file

---
+++ features/step_definitions/base_methods.rb
@@ -0,0 +1,14 @@
+def create_user username, password
+  %x[ wp user create "#{username}" "#{username}@chaifeng.com" --role=contributor --user_pass="#{password}" ]
+end
+
+def login username, password
+  visit "http://atdd.local/wp-admin/"
+  fill_in "user_login", with: username
+  fill_in "user_pass", with: password
+  click_on "Sign In"
+end
+
+def delete_user username
+  %x[wp user delete #{username} --yes]
+end
--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -5,28 +5,13 @@
   create_user username, password
 end
 
-def create_user username, password
-  %x[ wp user create "#{username}" "#{username}@chaifeng.com" --role=contributor --user_pass="#{password}" ]
-end
-
 当(/^使用用户名 "(.*?)" 和密码 "(.*?)" 登录$/) do |username, password|
   login username, password
-end
-
-def login username, password
-  visit "http://atdd.local/wp-admin/"
-  fill_in "user_login", with: username
-  fill_in "user_pass", with: password
-  click_on "Sign In"
 end
 
 那么(/^能够成功登录$/) do
   expect(title).to eq("Dashboard ‹ Specification By Example Workshop — WordPress")
   delete_user @username
-end
-
-def delete_user username
-  %x[wp user delete #{username} --yes]
 end
 
 那么(/^会因为密码错误而登录失败$/) do
