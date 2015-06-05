f8d38486ab6b44ef86c40ab125c8e19db7752a86
Chai Feng <chaifeng@odd-e.com>
2015-06-05 13:03:35 +0800

    remove unused steps

--- features/step_definitions/comment_steps.rb
+++ features/step_definitions/comment_steps.rb
@@ -1,22 +1,5 @@
 @current_time = ""
 
-当(/^我在评论文本框里输入 "(.*?)"$/) do |arg1|
-  @current_time = Time.now.to_s
-  step %Q[我在 id 为 "comment" 文本框里输入 "#{arg1}|#{@current_time}"]
-end
-
-那么(/^我会看到评论 "(.*?)"$/) do |arg1|
-  step %Q[我会看到页面上的文本 "#{arg1}|#{@current_time}"]
-end
-
-那么(/^我不会看到评论 "(.*?)"$/) do |arg1|
-  expect(page).to have_no_content("#{arg1}|#{@current_time}")
-end
-
-假如(/^批准了评论 "(.*?)"$/) do |arg1|
-  %x[wp comment approve $(wp comment list --format=ids --search="#{arg1}|#{@current_time}")]
-end
-
 当(/^普通用户提交了一个评论$/) do
   visit "http://atdd.local/wp-login.php"
   fill_in "user_login", with: "marry"
