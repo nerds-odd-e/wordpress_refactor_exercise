463fc73cdac10ad7c5fbb0e027578a26a28b789b
Chai Feng <chaifeng@odd-e.com>
2015-06-05 12:58:15 +0800

    pass the test, comment approved by a editor

--- features/step_definitions/comment_steps.rb
+++ features/step_definitions/comment_steps.rb
@@ -38,3 +38,14 @@ end
   expect(page).to have_no_content("a comment by subscriber")
   %x[wp comment delete "$(wp comment list --format=ids --post_id=9 --status=hold --search="a comment by subscriber")" --force]
 end
+
+当(/^这个评论被编辑审核通过$/) do
+  %x[wp comment approve "$(wp comment list --format=ids --post_id=9 --status=hold --search="a comment by subscriber")"]
+end
+
+那么(/^这个评论将被其他人公开看到$/) do
+  click_on "Log out"
+  visit "http://atdd.local/?p=9"
+  expect(page).to have_content("a comment by subscriber")
+  %x[wp comment delete "$(wp comment list --format=ids --post_id=9 --search="a comment by subscriber")" --force]
+end
