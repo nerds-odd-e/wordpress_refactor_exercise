1694c8561c0251c63d259bc1dbb87ed070c39db0
Chai Feng <chaifeng@odd-e.com>
2015-06-05 13:05:36 +0800

    remove unused steps in my_first_steps

--- features/step_definitions/my_first_steps.rb
+++ features/step_definitions/my_first_steps.rb
@@ -4,34 +4,3 @@ require 'capybara/mechanize'
 
 Capybara.default_driver = :mechanize
 Capybara.app = true
-
-假如(/^我打开了 URL "(.*?)"$/) do |arg1|
-  visit arg1
-end
-
-那么(/^我会看到页面的链接为 "(.*?)"$/) do |arg1|
-  expect(current_url).to eq(arg1)
-end
-
-那么(/^我会看到页面的标题为 "(.*?)"$/) do |arg1|
-  expect(title).to eq(arg1)
-end
-
-当(/^我在 id 为 "(.*?)" 文本框里输入 "(.*?)"$/) do |arg1, arg2|
-  fill_in arg1, with: arg2
-end
-
-当(/^我点击了 id 为 "(.*?)" 的按钮$/) do |arg1|
-  click_on arg1
-end
-
-那么(/^我会看到页面上的文本 "(.*?)"$/) do |arg1|
-  expect(page).to have_content(arg1)
-end
-
-当(/^我点击了 "(.*?)"$/) do |arg1|
-  click_on arg1
-end
-那么(/^我会看到 "(.*?)"$/) do |arg1|
-  step %Q[我会看到页面上的文本 "#{arg1}"]
-end
